03d8b37bda8f76a4c9a2b6787988e5559832d69c
NickSramcik <Nick.Sramcik@gmail.com>
2022-04-12 19:03:23 -0700

    daily challenge

--- 8kyu/summation.js
+++ 8kyu/summation.js
@@ -1,9 +1,14 @@
 // Write a program that finds the summation of every number from 1 to num. The number will always be a positive integer greater than 0.
 
+// Pass in a positive integer
+// Return a sum from 1 to the input
+
 // For example:
 
 // summation(2) -> 3
 // 1 + 2
+
+// Loop to the input and add 1 each iteration
 
 const summation = function (num) {
     let sum = 0
