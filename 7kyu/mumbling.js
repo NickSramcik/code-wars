81c11efaff2943c08341719fecaa06e7eb4c84df
NickSramcik <Nick.Sramcik@gmail.com>
2022-05-17 12:48:43 -0700

    daily challenge

---
+++ 7kyu/mumbling.js
@@ -0,0 +1,38 @@
+// This time no story, no theory. The examples below show you how to write function accum:
+
+// Examples:
+// accum("abcd") -> "A-Bb-Ccc-Dddd"
+// accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+// accum("cwAt") -> "C-Ww-Aaa-Tttt"
+// The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+//PREP
+
+//Pass in a string, return another string with capital first letters, separated with -, with each being the length of its index + 1
+// "bnm" -> "B-Nn-Mmm"
+
+
+//Split string into an array
+//Map array by repeating each element [index + 1] times
+//Capitalize first letter of each element
+//Join into string with - separator, return
+
+function accum(s) {
+    let arr = s.split('').map((e, i) => e.toLowerCase().repeat(i + 1))
+    return arr.map(e => e[0].toUpperCase() + e.slice(1)).join('-')
+}
+
+// Combine into one line
+
+const accum = s => s.split('').map((e, i) => 
+    e.toLowerCase().repeat(i + 1)).map(e => e[0].toUpperCase() + e.slice(1)).join('-')
+
+// Use repeat for the slice to condense
+
+function accum(s) {
+    return s.split('').map((e, i) => e[0].toUpperCase() + e.toLowerCase().repeat(i)).join('-')
+}
+
+// One line
+
+const accum = s => s.split('').map((e, i) => e[0].toUpperCase() + e.toLowerCase().repeat(i)).join('-')
