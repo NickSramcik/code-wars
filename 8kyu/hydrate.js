0a6d32ef98ad397ed70035b25fd61da46520cc5f
NickSramcik <Nick.Sramcik@gmail.com>
2022-06-16 17:31:35 -0700

    daily challenge

---
+++ 8kyu/hydrate.js
@@ -0,0 +1,28 @@
+// Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+
+// For example:
+
+// time = 3 ----> litres = 1
+
+// time = 6.7---> litres = 3
+
+// time = 11.8--> litres = 5
+
+//Pass in a positive number, could be a decimal
+//Return a whole number rounded down to smallest value
+// time = 9 -> 4
+//Divide the input by 2 and truncate, return
+
+function litres(time) {
+    return Math.trunc(time / 2);
+  }
+
+//One liner
+
+const litres = t => Math.trunc(t/2);
+
+//using floor
+
+const litres = t => Math.floor(t * 0.5);
