3bea122fefbf14f58714911bbb47752d3b3baba9
NickSramcik <Nick.Sramcik@gmail.com>
2022-06-24 20:21:07 -0700

    daily challenge

---
+++ 8kyu/sentenceSmash.js
@@ -0,0 +1,14 @@
+// Sentence Smash
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// Example
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+
+//Pass in an array of words
+//Return a string, each work put together in a sentence w/ spaces
+//['Ogres', 'are', 'like', 'onions'] -> 'Ogres are like onions'
+
+//Join the array with spaces inbetween using an array method
+
+// const smash = words => words.join(' ');
