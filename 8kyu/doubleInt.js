7c957d306367e667ab7b53acdf45253980ac6b9e
NickSramcik <Nick.Sramcik@gmail.com>
2022-08-08 17:07:03 -0700

    daily challenge

---
+++ 8kyu/doubleInt.js
@@ -0,0 +1,14 @@
+// Code as fast as you can! You need to double the integer and return it.
+
+
+// Pass in an integer
+// Return the integer doubled asap
+// 2 => 4
+
+// return 2* input
+
+const doubleInteger = i => 2 * i;
+
+// Another
+
+const doubleInteger = i => i+i;
