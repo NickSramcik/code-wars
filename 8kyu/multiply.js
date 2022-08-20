f4af734ab7bb98ea9d2f35b633d51dd9991f69fa
NickSramcik <Nick.Sramcik@gmail.com>
2022-08-19 18:57:51 -0700

    daily challenge

---
+++ 8kyu/multiply.js
@@ -0,0 +1,18 @@
+// Implement a function called multiply, which takes two numbers and returns their product:
+
+// multiply(2, 3) = 6
+// multiply(0, 188) = 0
+// multiply(85, 144) = 12240
+
+// Pass in 2 numbers
+// Return their products
+// (6, 9) -> 54
+// Multiply the inputs and return
+
+const multiply = (n1, n2) => n1 * n2;
+
+// One line functions don't have to be constants
+
+let multiply = (a, b) => a*b;
+
+// Probably not a good idea to do this though, in case something changes the function by mistake
