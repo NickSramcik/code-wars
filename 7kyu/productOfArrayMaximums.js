bf5dec902b3efb08e2453b2f3236691889cefb0c
NickSramcik <Nick.Sramcik@gmail.com>
2022-09-09 12:32:16 -0700

    daily challenge

---
+++ 7kyu/productOfArrayMaximums.js
@@ -0,0 +1,41 @@
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+// maxProduct ({8, 10 , 9, 7}, 3) ==>  return (720)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is  8 * 9 * 10 = 720 .
+// maxProduct ({10, 8, 3, 2, 1, 4, 10}, 5) ==> return (9600)
+// Explanation:
+// Since the size (k) equal 5 , then the subsequence of size 5 whose gives product of maxima is  10 * 10 * 8 * 4 * 3 = 9600 .
+// maxProduct ({-4, -27, -15, -6, -1}, 2) ==> return (4)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is  -4 * -1 = 4 .
+// maxProduct ({10, 3, -1, -27} , 3)  return (-30)
+// Explanation:
+// Since the size (k) equal 3 , then the subsequence of size 3 whose gives product of maxima is 10 * 3 * -1 = -30 .
+
+
+// Pass in an array of integers (length > 3) and input n
+// Return the product of the largest n numbers in that array
+// [1, 1, 4, 4], 2 -> 16
+
+// Sort the array, slice the maximum n numbers, and reduce the product 
+
+function maxProduct(arr, n) {
+    return arr.sort((a, b) => b - a).slice(0, n).reduce((a, c) => a * c, 1);
+}
+
+// One line
+
+const maxProduct = (arr, n) => arr.sort((a, b) => b - a).slice(0, n).reduce((a, c) => a * c, 1);
