a2f09e858630d704dc45ccdc261cba31128bcb10
NickSramcik <Nick.Sramcik@gmail.com>
2022-10-03 18:45:57 -0700

    daily challenge

---
+++ 8kyu/printArray.js
@@ -0,0 +1,23 @@
+// Input: Array of elements
+
+// ["h","o","l","a"]
+
+// Output: String with comma delimited elements of the array in th same order.
+
+// "h,o,l,a"
+
+// Note: if this seems too simple for you try the next level
+
+// Pass in an array of strings
+// Return a string of those elements in the same order
+// ["s", "h", "r", "e," "k"] -> "shrek"
+
+// Convert the array to a string and return
+
+function printArray(array){
+    return array.toString();
+  }
+
+// One line
+
+const printArray = a => String(a);
