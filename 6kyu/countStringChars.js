1d74aec53647605910d7f14136b766f76f0df5c6
NickSramcik <Nick.Sramcik@gmail.com>
2022-10-17 15:51:58 -0700

    daily challenge edit

--- 6kyu/countStringChars.js
+++ 6kyu/countStringChars.js
@@ -32,3 +32,10 @@ const count = (str, res = {}) => {
     [...str].forEach(e => res[e] = ++res[e] || 1);
     return res;    
 }
+
+// One line using a reduce
+const count = s => [...s].reduce((a, c) => (a[c] ? a[c] += 1 : a[c] = 1, a), {});
+
+// -~value will add 1 to the value, or set it to 1 if it is undefined
+const count = s => [...s].reduce((a, c) => (a[c] = -~a[c], a), {});
+
