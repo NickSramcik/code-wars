afb528991fde28b72b38692aaf820aecd52fcb91
NickSramcik <Nick.Sramcik@gmail.com>
2022-12-15 16:57:19 -0800

    daily challenge

---
+++ leetcode/pairedSums.js
@@ -0,0 +1,36 @@
+// Given an array of numbers, return all pairs that add up to a given sum. The numbers can be used more than once.
+
+// describe("Two Sum", () => {
+//  it("Should implement two sum", () => {
+//   assert.deepEqual(twoSum([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]]);
+//  });
+// });
+
+// Pass in an array of numbers, and another integer representing sum
+// Return an array of pairs from input array that add up to the input sum
+
+// [1, 2, 2, 3, 4], 4 -> [[2, 2], [3, 1]]
+
+// Create a hash map for the numbers in the array and an array for the pairs
+// Loop through the array. For each number, check if its required pair is in the map 
+// If it is, add the pair to the pairs array
+// Otherwise, add the first number to the map
+
+function pairedSums(arr, sum) {
+    const numsMap = {};
+    let pairs = [];
+  
+    for (num1 of arr) {
+        const num2 = sum - num1;
+
+        if (numsMap[num2]) {
+            pairs.push([num1, num2]);
+        }else numsMap[num1] = 1;
+    }return pairs;
+}
+
+// Test Cases
+
+console.log(pairedSums([1, 2, 2, 3, 4], 4), [[2, 2], [3, 1]])
+console.log(pairedSums([1, 4, 2, 5], 6), [[1, 5], [2, 4]])
+console.log(pairedSums([1, 10, 0, 15], 10), [[0, 10]])
