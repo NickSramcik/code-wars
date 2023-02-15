ccdd3cd8da554ef3e6ad80f3e5b8b489caf5c70c
NickSramcik <Nick.Sramcik@gmail.com>
2023-02-14 19:01:31 -0800

    daily challenge

---
+++ 6kyu/queenOfHeartsChallenge.js
@@ -0,0 +1,12 @@
+// Alice laughed. 'There's no use trying,' she said. 'One can't believe impossible things.'
+
+// I daresay you haven't had much practice,' said the Queen. 'When I was your age, I always did it for half-an-hour a day. Why, sometimes I've believed as many as six impossible things before breakfast.
+
+// Lewis Carroll, "Alice In Wonderland"
+
+// What To Do?
+// There's a secret phrase hidden somewhere. In order to pass the test, you have to find it.
+
+function solveRiddle() {
+	console.log('shrek');
+}
