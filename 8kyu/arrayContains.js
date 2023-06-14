12756aa53e23f6e232d3b1177a11af8442e1e452
NickSramcik <Nick.Sramcik@gmail.com>
2023-06-14 16:32:36 -0700

    daily challenge

---
+++ 8kyu/arrayContains.js
@@ -0,0 +1,15 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+// Pass in an array and a value of any type
+// Return boolean, whether the array includes input value
+// [1, 2, 3], 'shrek' -> false
+
+// Return if input is included in array
+
+function check(a, x) {
+    return a.includes(x);
+}
