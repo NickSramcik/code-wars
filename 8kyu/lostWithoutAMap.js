2f349d50f284f5923e79d1ef6a2d07d6a1a48030
NickSramcik <Nick.Sramcik@gmail.com>
2023-06-16 16:27:34 -0700

    daily challenge

---
+++ 8kyu/lostWithoutAMap.js
@@ -0,0 +1,14 @@
+// Given an array of integers, return a new array with each value doubled.
+
+// For example:
+
+// [1, 2, 3] --> [2, 4, 6]
+
+// Pass in an array of numbers
+// Return an array of numbers, each element doubled
+
+// Map the array, doubling each value
+
+function maps(arr) {
+    return arr.map(num => num * 2);
+}
