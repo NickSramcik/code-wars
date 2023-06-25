73c5e57c003130bee448c5c7fa78b8a1413ca0ad
NickSramcik <Nick.Sramcik@gmail.com>
2023-06-24 22:47:51 -0700

    daily challenge

---
+++ 8kyu/getAscii.js
@@ -0,0 +1,13 @@
+// Get ASCII value of a character.
+
+// For the ASCII table you can refer to http://www.asciitable.com/
+
+// pass in a single character string
+// Return a number, its character code
+// 'A' -> 65
+
+// Return the char code at index 0
+
+function getASCII(c) {
+    return c.charCodeAt(0);
+}
