ac786b536e7e4cb2f00c66b78d5d47f18edd063e
NickSramcik <Nick.Sramcik@gmail.com>
2023-07-14 22:58:39 -0700

    daily challenge

---
+++ 7kyu/circleArea.js
@@ -0,0 +1,17 @@
+// Complete the function circleArea so that it will return the area of a circle with the given radius. Round the returned number to two decimal places (except for Haskell). If the radius is not positive or not a number, return false.
+
+// Example:
+
+// circleArea(-1485.86);    //returns false
+// circleArea(0);           //returns false
+// circleArea(43.2673);     //returns 5881.25
+// circleArea(68);          //returns 14526.72
+// circleArea("number");    //returns false
+
+// Pass in a float numbaer, radius
+// Return a number, the area of the circle
+
+// If input is not a positve number return false
+// Return π * r ^ 2
+
+var circleArea = function (radius) {};
