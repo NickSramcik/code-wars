be755ab63183fd9ac0b469fd22309a42c2bab3da
NickSramcik <Nick.Sramcik@gmail.com>
2023-07-19 12:59:35 -0700

    daily challenge

---
+++ 7kyu/nicknameGenerator.js
@@ -0,0 +1,36 @@
+// Nickname Generator
+
+// Write a function, nicknameGenerator that takes a string name as an argument and returns the first 3 or 4 letters as a nickname.
+
+// If the 3rd letter is a consonant, return the first 3 letters.
+
+// nickname("Robert") //=> "Rob"
+// nickname("Kimberly") //=> "Kim"
+// nickname("Samantha") //=> "Sam"
+// If the 3rd letter is a vowel, return the first 4 letters.
+
+// nickname("Jeannie") //=> "Jean"
+// nickname("Douglas") //=> "Doug"
+// nickname("Gregory") //=> "Greg"
+// If the string is less than 4 characters, return "Error: Name too short".
+
+// Notes:
+
+// Vowels are "aeiou", so discount the letter "y".
+// Input will always be a string.
+// Input will always have the first letter capitalised and the rest lowercase (e.g. Sam).
+// The input can be modified
+
+// Pass in a string
+// Return a string that only has the first 3 or 4 letters of input
+// 'Kimberly' -> Kim
+
+// If string.length < 4 return 'Error: Name too short'
+// If the third letter is a vowel, return the first 4 letters
+// Otherwise, return the first 3
+
+function nicknameGenerator(name) {
+    if (name.length < 4) return 'Error: Name too short';
+    if ('aeiuo'.includes(name[2])) return name.slice(0, 4);
+    return name.slice (0, 3);
+}
