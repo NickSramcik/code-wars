da366a2cb921968b3af5898bafca3608089ccba0
NickSramcik <Nick.Sramcik@gmail.com>
2023-08-10 09:23:31 -0700

    daily challenge

---
+++ 8kyu/stringRepeat.py
@@ -0,0 +1,12 @@
+# Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+# Examples (input -> output)
+# 6, "I"     -> "IIIIII"
+# 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+# Pass in a positive integer n and a string
+# Return input string repeated n times
+
+def repeat_str(repeat, string):
+    return string * repeat
+    
