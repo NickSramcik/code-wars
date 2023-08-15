60c9f5e815f0cef9161fa6363109523f15583c9a
NickSramcik <Nick.Sramcik@gmail.com>
2023-08-14 19:02:49 -0700

    daily challenge

---
+++ 8kyu/boolToString.py
@@ -0,0 +1,10 @@
+# Implement a function which convert the given boolean value into its string representation.
+
+# Note: Only valid inputs will be given.
+
+# Pass in a boolean value
+# Return the input as a string
+# true -> "true"
+
+def boolean_to_string(b):
+    return str(b)
