e0cd811227d0cd8382ff90f051fd2ee9a4b37256
NickSramcik <Nick.Sramcik@gmail.com>
2023-09-09 11:20:29 -0700

    daily challenge

---
+++ 8kyu/filterOdds.js
@@ -0,0 +1,11 @@
+// Time to test your basic knowledge in functions! Return the odds from a list:
+
+// [1, 2, 3, 4, 5]  -->  [1, 3, 5]
+// [2, 4, 6]        -->  []
+
+// Pass in an array of numbers
+// Return an array of odd numbers
+
+// Filter the array for numbers with a remainder after a modulus with 2
+
+const odds = arr => arr.filter(n => n % 2);
