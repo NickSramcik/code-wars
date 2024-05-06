ae545bea5319163a93db393bca37fa6a7fb8eb68
NickSramcik <Nick.Sramcik@gmail.com>
2024-05-06 10:27:53 -0700

    daily challenge

---
+++ 7kyu/classySong.js
@@ -0,0 +1,57 @@
+// Your job is to create a class called Song.
+
+// A new Song will take two parameters, title and artist.
+
+// const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+
+// mountMoose.title => 'Mount Moose'
+// mountMoose.artist => 'The Snazzy Moose'
+// You will also have to create an instance method named howMany() (or how_many()).
+
+// The method takes an array of people who have listened to the song that day. The output should be how many new listeners the song gained on that day out of all listeners. Names should be treated in a case-insensitive manner, i.e. "John" is the same as "john".
+
+// Example
+// const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+
+// // day 1 (or test 1)
+// mountMoose.howMany(['John', 'Fred', 'BOb', 'carl', 'RyAn']); => 5
+// // These are all new since they are the first listeners.
+
+// // day 2
+// mountMoose.howMany(['JoHn', 'Luke', 'AmAndA']); => 2
+// // Luke and Amanda are new, john already listened to it the previous day
+// Also if the same person listened to it more than once a day it should only count them once.
+
+// Example
+// const mountMoose = new Song('Mount Moose', 'The Snazzy Moose');
+
+// // day 1
+// mountMoose.howMany(['John', 'joHN', 'carl']); => 2
+
+// class Song
+// args: title, artist
+// store: listener object
+// methods: howMany -> pass in array, return number, amount of listeners not already present in listener obj
+
+class Song{
+    constructor(title, artist) {
+        this.title = title;
+        this.artist = artist;
+        this.listeners = {};
+    }
+
+    howMany(arr) {
+        let newListeners = 0;
+        
+        for (let name of arr) {
+            // Lowercase the listener name for case insensitivity
+            name = name.toLowerCase();
+            if (!this.listeners[name]) {
+                this.listeners[name] = true;
+                newListeners++;
+            }
+        }
+
+        return newListeners;
+    }
+}
