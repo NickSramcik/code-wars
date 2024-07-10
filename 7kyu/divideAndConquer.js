a87416817743588e3a84c55c029e54e95c2c6b6c
NickSramcik <Nick.Sramcik@gmail.com>
2024-07-10 10:22:13 -0700

    daily challenge

---
+++ 7kyu/divideAndConquer.js
@@ -0,0 +1,17 @@
+// Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+// Return as a number.
+
+// Pass in an array of numbers and string numbers
+// Return a number
+
+// Let numSum be all numbers filtered from input array and reduced
+// Let stringSum be all string numbers filtered from input array and reduced
+// Return numSum - stringSum
+
+function divCon(arr) {
+    const numSum = arr.filter(x => typeof x == 'number').reduce((acc, cur) => acc + cur, 0);
+    const stringSum = arr.filter(x => typeof x == 'string').reduce((acc, cur) => acc + Number(cur), 0);
+
+    return numSum - stringSum;
+}
