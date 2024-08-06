2f3240268e8dc738b8c3d8a456c49c7f9c22f3af
NickSramcik <Nick.Sramcik@gmail.com>
2024-08-06 10:47:40 -0700

    daily challenge

---
+++ 7kyu/fullHouse.js
@@ -0,0 +1,23 @@
+// Given an array with exactly 5 strings "a", "b" or "c" (chars in Java, characters in Fortran), check if the array contains three and two of the same values.
+
+// Examples
+// ["a", "a", "a", "b", "b"] ==> true  // 3x "a" and 2x "b"
+// ["a", "b", "c", "b", "c"] ==> false // 1x "a", 2x "b" and 2x "c"
+// ["a", "a", "a", "a", "a"] ==> false // 5x "a"
+
+// Pass in an array
+// Return boolean
+
+// Let map be an empty object
+// Loop through input array and use map object to store frequency of each value
+// Return whether map object has two values of 2 & 3
+
+function checkThreeAndTwo(array) {
+    const map = {};
+
+    for (item of array) {
+        map[item] = map[item] + 1 || 1;
+    }
+
+    return Object.keys(map).some(key => map[key] == 2) && Object.keys(map).some(key => map[key] == 3);
+}
