4149fc5bae7a0c6e4155bf2f2d24a437f0a9a860
NickSramcik <Nick.Sramcik@gmail.com>
2024-10-18 10:09:08 -0700

    daily challenge

---
+++ 7kyu/reverseList.js
@@ -0,0 +1,10 @@
+// Write reverseList function that simply reverses lists.
+
+// Pass in an array
+// Return an array
+
+// Reverse the array and return the result
+
+function reverseList(arr) {
+    return arr.reverse();
+}
