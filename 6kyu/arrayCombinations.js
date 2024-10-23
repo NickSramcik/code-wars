04acf1f3b22af9f149e3d69036032257d58113cc
NickSramcik <Nick.Sramcik@gmail.com>
2024-10-23 11:11:52 -0700

    refactor

--- 6kyu/arrayCombinations.js
+++ 6kyu/arrayCombinations.js
@@ -18,14 +18,47 @@
 // Map the parent array
 // For each array, remove duplicates. Then reduce them, multiplying each length to get total combinations
 
-function solve(arr) {
+function shreksolve(arr) {
     return arr.map(e => [...new Set(e)]).reduce((a, c) => a * c.length, 1);
   };
 
 // Refactored into arrow function
 
-const solve = arr => arr.map(e => [...new Set(e)]).reduce((a, c) => a * c.length, 1);
+const oldersolve = arr => arr.map(e => [...new Set(e)]).reduce((a, c) => a * c.length, 1);
 
 // Use the size attribute of the set for the map, then reduce those
 
-const solve = arr => arr.map(e => new Set(e).size).reduce((a, c) => a * c, 1);
+const oldsolve = arr => arr.map(e => new Set(e).size).reduce((a, c) => a * c, 1);
+//
+
+// 1, 4, 5
+// 1, 4, 5
+
+// Pass in an array of subarrays
+// Return a number
+
+// Pass in: [[1,2],[4,4],[5,6,6]], get: 4
+// 1, 4, 5
+// 2, 4, 5
+// 1, 4, 6
+// 2, 4, 6
+
+// If we have a series of choices...
+// (# of options for choice 1) * (# of options for choice 2) * (# of options for choice 3) = total # of combinations
+// 2 * 1 * 2 = 4 
+
+
+// [[1,2],[4,4],[5,6,6]]
+// [[1,2], [4], [5, 6]] // no more duplicates! 
+// [{1, 2}, {4}, {5, 6}] // Array of sets, each subarray became a set
+// [2, 1, 2] // Array of set sizes
+
+function solve(arrays) {
+  return arrays.map(subarray => (new Set(subarray)).size).reduce((acc, cur) => cur * acc, 1);
+}
+
+// 1 * 2, 2 * 1, 2 * 2, 4
+
+// Test Cases
+
+console.log(solve([[1,2],[4,4],[5,6,6]]), 4);
