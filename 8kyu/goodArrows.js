6d25155743f17db461aa9c898a7b9fd16aa81c1c
NickSramcik <Nick.Sramcik@gmail.com>
2024-12-02 10:22:44 -0800

    daily challenge

---
+++ 8kyu/goodArrows.js
@@ -0,0 +1,16 @@
+// You have a quiver of arrows, but some have been damaged. The quiver contains arrows with an optional range information (different types of targets are positioned at different ranges), so each item is an arrow.
+// You need to verify that you have some good ones left, in order to prepare for battle:
+
+// anyArrows([{range: 5}, {range: 10, damaged: true}, {damaged: true}])
+// If an arrow in the quiver does not have a damaged status, it means it's new.
+
+// The expected result is a boolean, indicating whether you have any good arrows left.
+
+// Pass in an array of objects
+// Return boolean
+
+// Loop through input objects and return whether any have a damaged property set to false
+
+function anyArrows(arrows) {
+    return arrows.some(arrow => !arrow.damaged);
+}
