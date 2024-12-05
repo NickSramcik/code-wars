db26a2e8da7f17ef3a6b9fecdcd5094af8de3b3c
NickSramcik <Nick.Sramcik@gmail.com>
2024-12-05 10:43:32 -0800

    daily challenge

---
+++ 7kyu/deodorant.js
@@ -0,0 +1,32 @@
+// This program tests the life of an evaporator containing a gas.
+
+// We know the content of the evaporator (content in ml), the percentage of foam or gas lost every day (evap_per_day) and the threshold (threshold) in percentage beyond which the evaporator is no longer useful. All numbers are strictly positive.
+
+// The program reports the nth day (as an integer) on which the evaporator will be out of use.
+
+// Example:
+// evaporator(10, 10, 5) -> 29
+// Note:
+// Content is in fact not necessary in the body of the function "evaporator", you can use it or not use it, as you wish. Some people might prefer to reason with content, some other with percentages only. It's up to you but you must keep it as a parameter because the tests have it as an argument.
+
+// Pass in 3 numbers
+// Return 1 number
+
+// Let days be 0
+// let end_point be threshold % of original content
+// While content >= end_point:
+//   increment days by 1
+//   lower content by evap_per_day %
+// Return days
+
+function evaporator(content, evap_per_day, threshold) {
+    let days = 0;
+    const end_point = content * threshold / 100;
+
+    while (content >= end_point) {
+        days++;
+        content = content - content * evap_per_day / 100;
+    }
+
+    return days;
+}
