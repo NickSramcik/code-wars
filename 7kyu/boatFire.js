636780dc4050cbb88fbc436d4c3cc1d06f3a5e26
NickSramcik <Nick.Sramcik@gmail.com>
2025-01-13 10:16:23 -0800

    daily challenge

---
+++ 7kyu/boatFire.js
@@ -0,0 +1,15 @@
+// Enjoying your holiday, you head out on a scuba diving trip!
+
+// Disaster!! The boat has caught fire!!
+
+// You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action. Change any instance of "Fire" into "~~". Then return the string.
+
+// Pass in a string
+// Return a string
+
+// Replace all instances of 'Fire' with '~~'
+// Return result
+
+function fireFight(str) {
+    return str.replaceAll('Fire', '~~');
+}
