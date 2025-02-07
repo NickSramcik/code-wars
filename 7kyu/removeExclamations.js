2fb0b22a8ff1f5ee2b15f998e519843410060603
NickSramcik <Nick.Sramcik@gmail.com>
2025-02-07 11:15:45 -0800

    daily challenge

---
+++ 7kyu/removeExclamations.js
@@ -0,0 +1,49 @@
+// Remove words from the sentence if they contain exactly one exclamation mark. Words are separated by a single space, without leading/trailing spaces.
+
+// Examples
+// remove("Hi!") === ""
+// remove("Hi! Hi!") === ""
+// remove("Hi! Hi! Hi!") === ""
+// remove("Hi Hi! Hi!") === "Hi"
+// remove("Hi! !Hi Hi!") === ""
+// remove("Hi! Hi!! Hi!") === "Hi!!"
+// remove("Hi! !Hi! Hi!") === "!Hi!"
+
+// Pass in a string
+// Return a string
+
+
+// Loop through input string, split into an array of words
+//    If current word has exactly 1 !, change it ''
+//    Otherwise leave it alone
+// Join array back to string, space separator
+// Trim result to remove trailing spaces
+// Return result
+
+function removeOldOld(string) {
+    return string.split(' ').map(word => word.includes('!') && !word.includes('!!') ? '' : word).join(' ').trim();
+}
+
+// Refactored for readability
+
+function removeOld(string) {
+    return string.split(' ')
+                 .map(word => {
+                    if (word.includes('!') && !word.includes('!!')) return ''; // Remove word if it has one ! but not more
+                    else return word; 
+                 })
+                 .join(' ')
+                 .trim();
+}
+
+// Refactored w/ filter
+
+const exclamationCount = word => word.split('').filter(char => char == '!').length;
+const remove = string => string.split(' ').filter(word => exclamationCount(word) !== 1).join(' ');
+
+// Test Cases
+
+console.log(remove("Hi!") == '')
+console.log(remove("Hi Hi! Hi!") == 'Hi')
+console.log(remove("Hi! Hi!! Hi!") == 'Hi!!')
+console.log(remove("Hi! Hi!!! Hi!!!!") == 'Hi!!! Hi!!!!')
